c20fb8863f2dab7c38563b0d1aed0b487c80c6dd
Mohammed Ali <moh_benmousa@yahoo.com>
2020-08-16 13:35:58 -0400

    @vit-1231

---
+++ src/test/java/com/bitrix24/runners/mine.java
@@ -0,0 +1,9 @@
+package com.bitrix24.runners;
+
+public class mine {
+    public static void main(String[] args) {
+
+        String s = "hi";
+        System.out.println("\"How Are You There\" = " + s);
+    }
+}
